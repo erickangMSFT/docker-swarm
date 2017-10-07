5ae38b25e2d490a809a3fafccf1a012c36751c7e
Eric Kang <erickangMSFT@outlook.com>
2017-10-06 21:37:44 -0700

    docker init

--- docker-install.sh
+++ docker-install.sh
@@ -1,20 +1,18 @@
-apt-get update
+#!/bin/bash
 
-apt-get install \
+apt-get update && apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-apt-get update
-
-apt-get install docker-ce
+apt-get update && apt-get install -y docker-ce
 
 
